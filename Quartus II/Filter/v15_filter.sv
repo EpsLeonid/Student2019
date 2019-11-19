9bb5586b5c599c5763f089bff490bf1b23318ab1
Andrew <almo-alin@mail.ru>
2019-11-19 10:30:35 +0700

    fix shift

--- Quartus II/Filter/v15_filter.sv
+++ Quartus II/Filter/v15_filter.sv
@@ -1,5 +1,3 @@
-import package_settings::*;
-import v15_filter_parameters::*;
 
 module v15_filter(
 	input clk,
@@ -7,31 +5,44 @@ module v15_filter(
 	input [SIZE_ADC_DATA-1 : 0]	input_data,
 	output [SIZE_FILTER_DATA-1:0] output_data
 );
-	logic [v15_dataSize-1:0]d;
-	logic [v15_dataSize-1:0]p[1:0];
-	logic [v15_dataSize-1:0]r;
-	logic [v15_dataSize-1:0]s[1:0];
-	logic [SIZE_ADC_DATA-1:0]data[v15_bufferSize-1:0];
+	import package_settings::*;
+	import v15_filter_parameters::*;
+
+	reg [v15_dataSize-1:0]d;
+	reg [v15_dataSize-1:0]dl;
+	reg [v15_dataSize-1:0]dk;
+	reg [v15_dataSize-1:0]p[1:0];
+	reg [v15_dataSize-1:0]r;
+	reg [v15_dataSize-1:0]s[1:0];
+	reg [SIZE_ADC_DATA-1:0]data[v15_bufferSize-1:0];
 	always @(posedge clk) begin
 		if(!reset) begin
 			for(int i=0; i<v15_bufferSize ; i++)
-				data[i] <= 0;
+				begin
+					data[i] <= 0;
+				end
 
 			p[1] <= 0;
-
+			p[0] <=0;
 			s[1] <= 0;
+			s[0] <= 0;
+			output_data <=0;
 		end
 		else begin
-			for (int i=1;i<v15_bufferSize;i++)
-				data[i]<=data[i-1];
 			data[0]<=input_data;
+			for (int i=1;i<v15_bufferSize;i++)
+				begin
+					data[i]<=data[i-1];
+				end
 			s[1]<=s[0];
 			p[1]<=p[0];
-			d<=data[0]-data[v15_l-1]-data[v15_k-1]+data[v15_k+v15_l-1];
+			dl<=data[0]-data[v15_l-1];
+			dk<=dl-data[v15_k-1];
+			d<=dk+data[v15_k+v15_l-1];
 			p[0]<=p[1]+d;
 			r<=v15_M*d+p[0];
 			s[0]<=s[1]+r;
-			output_data	<=s[0][v15_dataSize-1:v15_dataSize-SIZE_ADC_DATA];
+			output_data	<=s[0]>>4;
 		end
 	end
 endmodule
